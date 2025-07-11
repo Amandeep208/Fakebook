b077934c7aa6c8c5f08e9356ef58c2a9a0679f39
amandeep <amandeep.singh@rstartec.com>
2025-07-11 14:51:43 +0530

    Edit and delete functionalitu fully Implemented and Edited status also added

--- fakebook/src/components/ChatBox.jsx
+++ fakebook/src/components/ChatBox.jsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import { BACKEND_URL } from "../config.js";
 import TopBar from "./TopBar.jsx";
 import useIsMobile from "../hooks/uselsMobile.js";
+import delete_img from "../assets/delete.svg";
 import edit from "../assets/edit.svg";
 
 function ChatBox() {
@@ -11,6 +12,7 @@ function ChatBox() {
   const [messages, setMessages] = useState([]);
   const [editingID, seteditingID] = useState(null);
   const [updatedmsg, setUpdatedmsg] = useState("");
+  const [deletemsg, setDeletemsg] = useState(null)
   const [input, setInput] = useState("");
   const scrollRef = useRef();
 
@@ -58,10 +60,35 @@ function ChatBox() {
     setUpdatedmsg(oldContent);
     seteditingID(id);
   };
+  const handleDeleteClick = (id) => {
+    setDeletemsg(id)
+  }
+
+  const handleDeleteSubmit = async (deletemsg_id) => {
+    const raw = ""
+    const res = await fetch(`${BACKEND_URL}/messages/delete/${deletemsg}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: raw,
+      redirect: "follow",
+      credentials: "include",
+    });
+
+    const result = await res.json();
+    console.log(result)
+    
+    if (result.success) {
+      setMessages(messages.filter(msg => msg._id !== deletemsg_id))
+      setDeletemsg(null);
+    } else {
+      console.error("Edit failed", result.message);
+    }
+
+  }
 
   const handleEditSubmit = async (editingID, updatedmsg) => {
     const res = await fetch(`${BACKEND_URL}/messages/edit`, {
-      method: "POST",
+      method: "PATCH",
       headers: {
         "Content-Type": "application/json",
       },
@@ -113,33 +140,75 @@ function ChatBox() {
           {messages.map((msg, i) => (
             <div
               key={msg._id}
-              className={`mb-2 flex ${msg.sender === selectedUser.username ? "justify-start" : "justify-end"}`}
+              className={`group flex-1 items-center mb-2 flex ${msg.sender === selectedUser.username ? "justify-start" : "justify-end"}`}
             >
-              <div
-                className={`group relative px-4 py-2 rounded-lg min-h-[3rem] min-w-[10rem] max-w-[70%] ${
-                  msg.sender === selectedUser.username
-                    ? "bg-gray-200 text-left"
-                    : "bg-purple-200 text-left"
-                }`}
-              >
-                <div>{msg.content}</div>
+              {/* Delete Button */}
+              {msg.sender !== selectedUser.username && (
 
-                {/* Timestamp */}
-                <div className="text-xs text-right mt-auto ml-4">
-                  {(new Date(msg.createdAt)).getHours().toString().padStart(2, "0")}:
-                  {(new Date(msg.createdAt)).getMinutes().toString().padStart(2, "0")}
-                </div>
-
-                {/* Edit Button (if user is sender) */}
-                {msg.sender !== selectedUser.username && (
+                <div
+                  className=" group ml-2 border-2  h-7 w-7 rounded-full bg-[#e5e7eb] opacity-0 translate-x-4 hover:border-[#4f4f51] group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300 ease-in-out  flex items-center"
+                // className="hidden group-hover:flex items-center transition-all duration-3000 transform translate-x-4 group-hover:translate-x-0 opacity-0 group-hover:opacity-100 ml-2"
+                >
                   <button
+                    onClick={() => handleDeleteClick(msg._id)}
+                    className="m-auto"
+                  >
+                    <img src={delete_img} alt="edit" className="w-4" />
+                  </button>
+                </div>
+              )}
+
+              {/*Edit Button*/}
+              {msg.sender !== selectedUser.username && (
+
+                <div
+                  className="ml-2  opacity-0 border-2 rounded-full bg-[#e5e7eb] h-7 w-7 mr-2 translate-x-4 hover:border-[#4f4f51] group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300 ease-in-out flex items-center"
+                // className="hidden group-hover:flex items-center transition-all duration-3000 transform translate-x-4 group-hover:translate-x-0 opacity-0 group-hover:opacity-100 ml-2"
+                >
+                  <button
+                    className="m-auto"
+
                     onClick={() => handleEditClick(msg._id, msg.content)}
-                    className="hidden group-hover:block absolute top-1 right-1"
+                  // className="transition duration-3000 ease-in-out hidden group-hover:block scale-105"
                   >
                     <img src={edit} alt="edit" className="w-4" />
                   </button>
+                </div>
+              )}
+
+
+              <div
+                className={`group relative  px-4 py-2 rounded-lg  min-w-[10rem] max-w-[70%] ${msg.sender === selectedUser.username
+                  ? "bg-gray-200 text-left"
+                  : "bg-purple-200 text-left"
+                  }`}
+              >
+                <div
+                  className="cursor-default"
+                >{msg.content}</div>
+
+                {/* Timestamp */}
+                <div className="flex justify-end gap-1 ">
+
+                {msg.__v > 0 && (
+                  <div className="text-xs text-left opacity-50 cursor-default">
+                  Edited
+                </div>
                 )}
+                <div className="text-xs text-right mt-auto cursor-default">
+                  {(new Date(msg.createdAt)).getHours().toString().padStart(2, "0")}:
+                  {(new Date(msg.createdAt)).getMinutes().toString().padStart(2, "0")}
+                </div>
+                </div>
+                
+
+
+
+
               </div>
+
+
+
             </div>
           ))}
           <div ref={scrollRef}></div>
@@ -176,8 +245,8 @@ function ChatBox() {
               value={updatedmsg}
               onChange={(e) => setUpdatedmsg(e.target.value)}
               onKeyDown={(e) => {
-              if (e.key === "Enter") handleEditSubmit(editingID,updatedmsg);
-            }}
+                if (e.key === "Enter") handleEditSubmit(editingID, updatedmsg);
+              }}
               className="w-full px-4 py-2 border rounded-lg mb-4 outline-none focus:ring-2 focus:ring-purple-400"
               autoFocus
             />
@@ -193,6 +262,34 @@ function ChatBox() {
                 className="px-4 py-2 rounded bg-purple-500 text-white hover:bg-purple-600"
               >
                 Save
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+
+
+
+
+
+      {/* Trigering Delete Confirm */}
+      {deletemsg && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-40">
+          <div className="bg-white rounded-xl shadow-xl p-6 w-full max-w-md mx-4">
+            <h2 className="text-xl font-semibold mb-4">Do you want to Delete Message</h2>
+            <div className="flex justify-end space-x-3">
+              <button
+                onClick={() => setDeletemsg(null)}
+                className="px-4 py-2 rounded bg-gray-200 hover:bg-gray-300"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={() => handleDeleteSubmit(deletemsg)}
+                className="px-4 py-2 rounded bg-purple-500 text-white hover:bg-purple-600"
+              >
+                Delete
               </button>
             </div>
           </div>
