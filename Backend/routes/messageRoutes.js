163bd13f904c211508faa0562c60069df757c405
Akshay <akshay.singh@rstartec.com>
2025-07-11 14:07:27 +0530

    Added /delete endpoint, implemented .patch() and .delete(), updated postman collection

--- Backend/routes/messageRoutes.js
+++ Backend/routes/messageRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { isAuthenticated } = require("../middleware/authMiddleware");
-const { messageFetch, sendMessage, editMessage } = require("../controllers/messageController");
+const { messageFetch, sendMessage, editMessage, deleteMessage } = require("../controllers/messageController");
 const router = express.Router();
 
-router.post("/send", isAuthenticated, sendMessage)
-router.get("/:to", isAuthenticated, messageFetch)
-router.post("/edit", isAuthenticated, editMessage)
+router.post("/send", isAuthenticated, sendMessage);
+router.get("/:to", isAuthenticated, messageFetch);
+router.patch("/edit", isAuthenticated, editMessage);
+router.delete("/delete/:msgID", isAuthenticated, deleteMessage);
 
 module.exports = router;
